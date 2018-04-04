cb0306b80d8e4b6d4ac356fcd7bc9a33fe891a87
liesugahara <lie.sugahara@gmail.com>
2018-04-04 17:03:12 -0500

    Update refresh.php

--- refresh.php
+++ refresh.php
@@ -7,7 +7,7 @@
 						// Datos de la base de datos
 						$usuario = "liesugahara";
 						$password = "Lccjhg1995";
-						$servidor = "dbtest.cakorcerkah4.us-east-2.rds.amazonaws.com:3306";
+						$servidor = "mydb.cakorcerkah4.us-east-2.rds.amazonaws.com:3306";
 						$basededatos = "Test";
 						
 						// creación de la conexión a la base de datos con mysql_connect()
