76a6ad156f73aaf5b6b30ac65b864469e8073d2b
liesugahara <lie.sugahara@gmail.com>
2018-04-04 16:09:54 -0500

    Update refresh.php

--- refresh.php
+++ refresh.php
@@ -1,22 +1,22 @@
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.js"></script>
+﻿<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.js"></script>
 <?php
 	// Ejemplo de conexión a base de datos MySQL con PHP.
 						//
 						// Ejemplo realizado por Oscar Abad Folgueira: http://www.oscarabadfolgueira.com y https://www.dinapyme.com
 						
 						// Datos de la base de datos
-						$usuario = "root";
-						$password = "";
-						$servidor = "localhost";
-						$basededatos = "bd";
+						$usuario = "ednp";
+						$password = "Diseno2018";
+						$servidor = "syrus.cf58lmtunvyx.us-east-2.rds.amazonaws.com:3306";
+						$basededatos = "syrusR";
 						
 						// creación de la conexión a la base de datos con mysql_connect()
-						$conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base de datos");
+						$conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
 						
 						// Selección del a base de datos a utilizar
 						$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
 						// establecer y realizar consulta. guardamos en variable.
-						$consulta = "SELECT * FROM vehicle_info";
+						$consulta = "SELECT * FROM vehicle_info order by id desc limit 1";
 						$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
 						
 						// Motrar el resultado de los registro de la base de datos
