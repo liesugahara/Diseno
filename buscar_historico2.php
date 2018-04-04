582fd07e74e8ab85ad0d0906b2f752ac9d0e21bf
liesugahara <lie.sugahara@gmail.com>
2018-04-04 16:11:56 -0500

    Create buscar_historico2.php

---
+++ buscar_historico2.php
@@ -0,0 +1,44 @@
+<?php 
+
+	$usuario = "ednp";
+	$password = "Diseno2018";
+	$servidor = "syrus.cf58lmtunvyx.us-east-2.rds.amazonaws.com:3306";
+	$basededatos = "syrusR";
+	$mensaje1 = "";
+	$mensaje = "";
+	$info = "";
+	$info1 = "";
+
+	
+	
+					
+	// creación de la conexión a la base de datos con mysql_connect()
+	$conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
+						
+	// Selección del a base de datos a utilizar
+	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+						
+						
+	
+		$query = "SELECT * FROM vehicle_info ";
+		$resultado = mysqli_query( $conexion, $query ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+		
+		
+		 $data = array();
+         while($resultados = mysqli_fetch_array($resultado)) {
+			
+			$data[] = $resultados;		
+			//Output
+			
+		};//Fin while $resultados
+	
+	echo json_encode($data);
+	
+	
+	
+	
+
+	
+	
+	
+?>
