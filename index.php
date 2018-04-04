c1124ed234238b951ce95ab324efd780c76d0ee3
liesugahara <lie.sugahara@gmail.com>
2018-04-04 13:18:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <!-- COMIENZO DEL CODIGO -->
-<title>Web Position</title>
+<title>TEST</title>
 <link type="text/css" rel="stylesheet" href="estilos-div.css" />
 <meta charset="utf-8">
 	
@@ -23,8 +23,8 @@
 		
 			<body>
 				<div id = "second_container" >
-						<center><fieldset style="width:30%;color:black;"><!--DEMARCACIÓN DEL DIV "Contenido"-->
-						<!--DIV DONDE SE MUESTRA LA INFORMACIÓN LATITUD,LONGITUD,HORA-->
+						<center><fieldset style="width:30%;color:black;"><!--DEMARCACIÃ“N DEL DIV "Contenido"-->
+						<!--DIV DONDE SE MUESTRA LA INFORMACIÃ“N LATITUD,LONGITUD,HORA-->
 							<legend style = "background:#BDBDBD"><b>Information</b></legend>
 							<div id="contenido" style = "background:#BDBDBD" >
 									
@@ -35,7 +35,7 @@
 
 						<!--TITULO DEL MAPA	-->
 						<center><h1 style = "color:white;font: oblique bold 120% cursive;font-size:20px">DISPLAYING POSITION </h1></center>
-						<!--CODIGO PHP DONDE IMPORTAN DATOS DEL ARCHIVO REFRESH.PHP, PERO ELIMINANDO LA INFORMACIÓN QUE MUESTRA-->
+						<!--CODIGO PHP DONDE IMPORTAN DATOS DEL ARCHIVO REFRESH.PHP, PERO ELIMINANDO LA INFORMACIÃ“N QUE MUESTRA-->
             <?php 
 							ob_start();
 							include 'refresh.php';
@@ -57,22 +57,22 @@
 									var map;
 									var marker;
 									var coordinates = [];
-									// FUNCIÓN QUE DESPLAZA EL MARCADOR CUANDO RECIBE LAS NUEVAS COORDENADAS
+									// FUNCIÃ“N QUE DESPLAZA EL MARCADOR CUANDO RECIBE LAS NUEVAS COORDENADAS
 									function moveToLocation(lat, lng){
 											var center = new google.maps.LatLng(lat, lng);
 											// using global variable:
 											map.panTo(center);
 									}
-									// FUNCIÓN PRINCIPAL QUE SE INICIALIZA EN EL MAPA. ESTÁ DECLARADO EN LA API KEY
+									// FUNCIÃ“N PRINCIPAL QUE SE INICIALIZA EN EL MAPA. ESTÃ DECLARADO EN LA API KEY
 									function initMap() {
-									//DECLARACIÓN DE LAS VARIABLES DE POSICIÓN Y PARÁMETROS NECESARIOS PARA EL MAPA
+									//DECLARACIÃ“N DE LAS VARIABLES DE POSICIÃ“N Y PARÃMETROS NECESARIOS PARA EL MAPA
 										var uluru = {lat:parseFloat('<?php echo $lat;?>'),lng: parseFloat('<?php echo $lon;?>')};
 										var parameters = {
 										  zoom: 15,
 										  center: uluru
 										};
 										map = new google.maps.Map(document.getElementById('map'),parameters) 
-											//MARCADOR INICIAL EN LA POSICIÓN INICIAL DE LA BASE DE DATOS
+											//MARCADOR INICIAL EN LA POSICIÃ“N INICIAL DE LA BASE DE DATOS
 											
 											/* var marker = new google.maps.Marker({
 											  position: uluru,
@@ -80,14 +80,14 @@
 											}); */
 										addMarker(new google.maps.LatLng(uluru),map)
 									}   
-									//FUNCIÓN QUE EXTRAE LOS VALORES DE POSICIÓN DEL JSON QUE SE GENERA EN LOCATION.PHP								
+									//FUNCIÃ“N QUE EXTRAE LOS VALORES DE POSICIÃ“N DEL JSON QUE SE GENERA EN LOCATION.PHP								
 									setInterval(function reload_map(){
-										//INSTRUCCIÓN NECESARIA PARA ACTUALIZAR LA INFORMACIÓN EN BACKGROUND
+										//INSTRUCCIÃ“N NECESARIA PARA ACTUALIZAR LA INFORMACIÃ“N EN BACKGROUND
 										$.ajax({
 													url: 'location.php',
 													dataType: "json",
 													success: function (data){
-													//EXTRACCIÓN DE VALORES DEL OBJETO DE TIPO JSON IMPORTADO DE LOCATION.PHP	
+													//EXTRACCIÃ“N DE VALORES DEL OBJETO DE TIPO JSON IMPORTADO DE LOCATION.PHP	
 													var obj_que =	jQuery.parseJSON(JSON.stringify(data));
 													$(jQuery.parseJSON(JSON.stringify(data))).each(function() {
 														//NUEVA LATITUD Y LONGITUD
@@ -118,9 +118,9 @@
 															console.log('Error: ' + json_obj);
 													}
 												});
-											//TIEMPO DE ACTUALIZACIÓN DEL MARCADOR: 5 SEGUNDOS
+											//TIEMPO DE ACTUALIZACIÃ“N DEL MARCADOR: 5 SEGUNDOS
 											},2000);
-											//FUNCIÓN QUE AGREGA EL MARCADOR
+											//FUNCIÃ“N QUE AGREGA EL MARCADOR
 											function addMarker(latLng, map) {
 												marker = new google.maps.Marker({
 														position: latLng,
