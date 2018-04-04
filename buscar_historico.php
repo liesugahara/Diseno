6da83e60e26cf07aa9f7d87307e41c61247d08cc
liesugahara <lie.sugahara@gmail.com>
2018-04-04 16:45:39 -0500

    Update buscar_historico.php

--- buscar_historico.php
+++ buscar_historico.php
@@ -1,9 +1,9 @@
 <?php 
 
-	$usuario = "ednp";
-	$password = "Diseno2018";
-	$servidor = "syrus.cf58lmtunvyx.us-east-2.rds.amazonaws.com:3306";
-	$basededatos = "syrusR";
+	$usuario = "liesugahara";
+	$password = "Lccjhg1995";
+	$servidor = "dbtest.cakorcerkah4.us-east-2.rds.amazonaws.com:3306";
+	$basededatos = "Test";
 	$date1 = $_POST['valorBusqueda'];
 	$date2 = $_POST['valorBusqueda2'];
 	$mensaje1 = "";
